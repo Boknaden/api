17e8f7ef7804a12a9c64b733b4ebc3665049207f
Nic <fristurt@hotmail.com>
2017-02-17 15:33:01 +0100

    updated ping

--- api/ping.js
+++ api/ping.js
@@ -1,7 +1,7 @@
 exports.get = function (req, res) {
     if (process.env.DEBUG) {
-        res.status(200).send('Pong')
+        res.status(200).send({payload: 'Pong'})
     } else {
-        res.status(404).send();
+        res.status(404).send()
     }
 }
