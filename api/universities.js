7d455d6c7edc927e457462a5615003ae48d92345
Nic <fristurt@hotmail.com>
2017-03-07 23:06:18 +0100

    added universities endpoint

---
+++ api/universities.js
@@ -0,0 +1,47 @@
+var shared = require('./_shared')
+function getUniversities (req, res) {
+    req.service.mysql.query(
+        'SELECT universityid, universityname, longitude, latitude, createddate FROM universities',
+        function (err, result, fields) {
+        if (err) {
+            res.send({err: err})
+            return
+        }
+        if (result.length === 0) {
+            res.send({err: 'No results.'})
+        } else {
+            res.send({payload: result})
+        }
+    })
+}
+
+function newUniversity (req, res) {
+    // var fields          = ["universityid", "universityname", "longitude", "latitude"]
+    // var valuesNotEmpty  = shared.checkEmptyValues(req.body, fields)
+    // var query           = "INSERT INTO universities ("+ fields.join(',') +") VALUES ("+ shared.genQuestionMarks(fields) +")"
+    //
+    // if (!valuesNotEmpty) {
+    //     res.send({err: 'Not all parameters specified.'})
+    //     return
+    // }
+    //
+    // req.service.mysql.query({
+    //     sql: query,
+    //     timeout: 10000,
+    //     values: [parseInt(req.body.universityid),req.body.universityname.trim(),req.body.longitude,req.body.latitude],
+    // }, function (err, results, fields) {
+    //     if (err) {
+    //         res.send({err: err, results: results, fields: fields, data: req.body})
+    //         return
+    //     }
+    //     res.send({payload: results})
+    // })
+
+    res.send({payload: 'Nice try, dude'})
+
+}
+
+module.exports = {
+    get: getUniversities,
+    post: newUniversity,
+}
