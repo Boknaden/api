364eea65e71c1f958a0c5e5766f4d12014c93127
Nic <nichlas.torgersen@gmail.com>
2017-11-23 17:34:25 +0100

    updated config

--- config-ex.js
+++ config-ex.js
@@ -3,20 +3,26 @@ module.exports = {
         host: 'localhost',
         user: 'root',
         password: '',
-        port: 3307,
-        database: 'boknaden_test',
+        port: 3306,
+        database: '',
     },
     security: {
-        saltRounds: 10, // hva det "koster" å utføre hashing (høyere tall er bedre)
-        secret: 'boknadensigneringsnokkel',
-        tokenExpiration: '48h', // token er gyldig i 48 timer
+        saltRounds: 10, // hva det "koster" å utføre hashing (høyere tall er bedre fordi hashingen tar lenger tid, men krever derfor mer datakraft)
+                        // jo lenger tid som brukes på hashing og compare av passord, jo lavere er risikoen for vellykkede bruteforce-angrep
+        secret: '',
+        tokenExpiration: '72h', // token er gyldig i 48 timer
     },
     email: {
-        service: 'gmail',
-        user: 'email',
-        pass: 'password',
+        service: '',
+        user: '',
+        pass: '',
         defaults: {
             from: '',
         }
+    },
+    imgur: {
+        clientId: '',
+        clientSecret: '',
+        apiUrl: 'https://api.imgur.com/3/'
     }
 }
